d5a9d492f11f98a15916d66f30b8d754a266015f
Nikola <37514211+nikola011381@users.noreply.github.com>
2022-07-26 21:47:12 +0200

    commit

--- cypress/integration/account/singUpTest.js
+++ cypress/integration/account/singUpTest.js
@@ -1,6 +1,5 @@
 /// <reference types="Cypress" />
 
-const { should } = require("chai");
 
 describe('SingUp Test suite', function() {
     it('1. Navigate to Sing Up page', function() {
@@ -10,19 +9,40 @@ describe('SingUp Test suite', function() {
     });
 
     it('2. Click on Sing-up while all fields are not populated', function() {
-       cy.get('.MuiButton-label').click();
-       cy.get('#firstName-helper-text').should('have.text','First Name is required'); 
+        cy.get('[data-test="signup-submit"]').click();
+      cy.get('#firstName-helper-text').should('have.text','First Name is required'); 
     });
 
     it('3. Check if all sign up fields are mandatory and have proper error message', function() {
-        //TODO
+        cy.get('#firstName').focus().blur()
+        cy.get('#firstName-helper-text').should('have.text','First Name is required');
+        cy.get('#lastName').focus().blur()
+        cy.get('#lastName-helper-text').should('have.text','Last Name is required');
+        cy.get('#username').focus().blur()
+        cy.get('#username-helper-text').should('have.text','Username is required');
+        cy.get('#password').focus().blur()
+        cy.get('#password-helper-text').should('have.text','Enter your password');
+        cy.get('#confirmPassword').focus().blur()
+        cy.get('#confirmPassword-helper-text').should('have.text','Confirm your password');
+       
+
     });
 
     it('4. Populate all sign up fields and Sing Up button should be enabled', function() {
-        //TODO
+        cy.get('#firstName').type('first name')
+        cy.get('#lastName').type('last name')
+        cy.get('#username').type('username')
+        cy.get('#password').type('password')
+        cy.get('#confirmPassword').type('password')
+        cy.get('[data-test="signup-submit"]').should('be.enabled');
+
     });
 
     it('5. Navigate back to Sign In page', function() {
-        //TODO
+      //  cy.get('[data-test="signup"]').click();
+      cy.get('[data-test="signup-submit"]').click();
+
+        cy.get('.MuiGrid-root > a').click()
+        cy.get('[class="MuiTypography-root MuiTypography-h5"]').contains('Sign in')
     });
 });
